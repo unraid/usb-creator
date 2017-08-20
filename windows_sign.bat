1adbe812e126d641ea5cb88ca68900a547d2868b
Eric Schultz <eric@startuperic.com>
2017-08-19 19:15:42 -0500

    added windows_sign.bat

---
+++ windows_sign.bat
@@ -0,0 +1,22 @@
+@echo off
+rem ################################################################################
+rem #      This file is part of unRAID USB Creator - https://github.com/limetech/usb-creator
+rem #      Copyright (C) 2013-2015 RasPlex project
+rem #      Copyright (C) 2016 Team LibreELEC
+rem #      Copyright (C) 2017 Lime Technology, Inc
+rem #
+rem #  unRAID USB Creator is free software: you can redistribute it and/or modify
+rem #  it under the terms of the GNU General Public License as published by
+rem #  the Free Software Foundation, either version 2 of the License, or
+rem #  (at your option) any later version.
+rem #
+rem #  unRAID USB Creator is distributed in the hope that it will be useful,
+rem #  but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem #  GNU General Public License for more details.
+rem #
+rem #  You should have received a copy of the GNU General Public License
+rem #  along with unRAID USB Creator.  If not, see <http://www.gnu.org/licenses/>.
+rem ################################################################################
+
+signtool sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 /a release\unRAID.USB.Creator.Win32.exe
